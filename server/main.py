f1858a25f3c0aaa2d5c73dae54d73b3cbd843d5a
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-11 23:02:00 +0100

    create route to handler creating a user

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-
+from lib import create_user, view_post, like_post
 from fastapi import Request, FastAPI
 
 app = FastAPI()
@@ -7,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 async def _handle_root(req: Request):
     return {"msg": "It works super good..."}
+
+
+@app.post("/make-user")
+async def handler_make_user(req: Request):
+    body = await req.json()
+    user_name = body.get("name", "anonymous")
+    result = create_user(user_name)
+    return result
