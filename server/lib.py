299f44c5175c6ad43097bf0af09d26e3564f60b2
JosiasAurel <ndjosiasaurel@gmail.com>
2021-10-11 22:58:54 +0100

    add handler to view post

--- server/lib.py
+++ server/lib.py
@@ -9,7 +9,7 @@ users = deta.Base("users")
 
 def create_user(name: str = "anonymous"):
     try:
-        result = users.put({"name": name})
+        result = users.put({"name": name, "posts": []})
         return result
     except:
         return "Failed"
@@ -17,7 +17,16 @@ def create_user(name: str = "anonymous"):
 
 def create_post(text: str, image_link: str):
     try:
-        posts.put({"text": text, "image": image_link})
+        posts.put({"text": text, "image": image_link, "likes": 0})
+        return "Success"
+    except:
+        return "Failed"
+
+
+def view_post(user_id: str, post_id: str, time_spent: int):
+    try:
+        users.update({"posts": users.util.append(
+            {"post": post_id, "time": time_spent})}, user_id)
         return "Success"
     except:
         return "Failed"
